9cd7caa6aa58c1eddaae45d52332c6ccaa52f2fb
charles-blackwell-ladd <45081734+charles-blackwell-ladd@users.noreply.github.com>
2018-11-15 14:04:45 -0800

    Create i_love_my_family.py

---
+++ i_love_my_family.py
@@ -0,0 +1,16 @@
+family = ["grandma",
+          "mommy",
+          "Elaine",
+          "daddy",
+          "grandpa",
+          "nina",
+          "papa",
+          "lindsey",
+          "james",
+          "lilly",
+          "john",
+          "layla"
+         ]
+
+for person in family:
+    print("i love " + person)
